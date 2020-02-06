381c772d8655bbada9d6973dfd0ad5034946aa9a
jomhak <50214404+jomhak@users.noreply.github.com>
2020-02-06 15:34:16 +0200

    Initial commit

---
+++ bookstore/src/main/java/hh/springproject/bookstore/webcontrol/BookController.java
@@ -0,0 +1,13 @@
+package hh.springproject.bookstore.webcontrol;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+	@GetMapping("/index")
+	public String showIndex() {
+		return "index";
+	}
+}

743c5cec0aa0f89b9203e4a898ff168faf1391f5
jomhak <50214404+jomhak@users.noreply.github.com>
2020-03-12 14:07:36 +0200

    Additions

--- bookstore/src/main/java/hh/springproject/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/hh/springproject/bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 
 import hh.springproject.bookstore.domain.Book;
 import hh.springproject.bookstore.domain.BookRepository;
+import hh.springproject.bookstore.domain.Category;
+import hh.springproject.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -21,14 +23,27 @@ public class BookstoreApplication {
 	}
 
 	
-	//testidata h2-tietokantaa varten
+	//test data for H2
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository bookRepository) {
+	public CommandLineRunner bookDemo(BookRepository bookRepository, CategoryRepository categoryRepository) {
 		return (args) -> {
-			log.info("Save some sample books");
-			bookRepository.save(new Book("Book title 1", "Author 1", 2010, "ISBN 1", 15.99));
-			bookRepository.save(new Book("Book title 2", "Author 2", 2012, "ISBN 2", 99.90));
+			log.info("Save some categories");
+			categoryRepository.save(new Category("Music"));
+			categoryRepository.save(new Category("Poetry"));
+			categoryRepository.save(new Category("History"));
 			
+			log.info("Save some sample books");
+			bookRepository.save(new Book("Book title 1", "Author 1", 2010, "ISBN 1", 15.99, categoryRepository.findByName("Music")));
+			bookRepository.save(new Book("Book title 2", "Author 2", 2012, "ISBN 2", 99.90, categoryRepository.findByName("History")));
+			
+			
+			
+			
+		log.info("Fetch all the categories");	
+		for (Category category : categoryRepository.findAll()) {
+			log.info(category.toString());
+		}
+		
 			
 		log.info("Fetch all the books");	
 		for (Book book : bookRepository.findAll()) {
--- bookstore/src/main/java/hh/springproject/bookstore/webcontrol/BookController.java
+++ bookstore/src/main/java/hh/springproject/bookstore/webcontrol/BookController.java
@@ -1,6 +1,7 @@
 package hh.springproject.bookstore.webcontrol;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,33 +9,51 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.springproject.bookstore.domain.Book;
 import hh.springproject.bookstore.domain.BookRepository;
+import hh.springproject.bookstore.domain.CategoryRepository;
 
 @Controller
 public class BookController {
 
 	@Autowired
-	BookRepository bookRepository;
+	private BookRepository bookRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
 
+	// Index
 	@GetMapping("/")
 	public String showIndex() {
 		return "index";
 	}
 	
+	// Add book
 	@GetMapping("/addbook")
 	public String showAddbook(Model model) {
 		model.addAttribute("book", new Book());
+		model.addAttribute("categories", categoryRepository.findAll());
 		return "addbook";
 	}
 	
+	// Save book
 	@PostMapping("/savebook")
 	public String saveBook(Book book) {
 		bookRepository.save(book);
 		return "redirect:booklist";
 	}
 
+	// List all books
 	@GetMapping("/booklist")
 	public String getAllBooks(Model model) {
 		// List books from database
@@ -43,16 +62,33 @@ public class BookController {
 		return "booklist";
 	}
 	
+	// Delete specific book
 	@GetMapping("/deletebook/{id}")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		bookRepository.deleteById(bookId);
 		return "redirect:../booklist";
 	}
 	
+	// Edit specific book
 	@GetMapping("/editbook/{id}")
 	public String editBook(@PathVariable("id") Long bookId, Model model) {
 		model.addAttribute("book", bookRepository.findById(bookId));
+		model.addAttribute("categories", categoryRepository.findAll());
 		return "editbook";
+	}
+	
+	// Rest methods
+	
+	//Rest: all books
+	@GetMapping("/books")
+	public @ResponseBody List<Book> jsonBook() {
+		return (List<Book>) bookRepository.findAll();
+	}
+	
+	//Rest: specific book
+	@GetMapping("/books/{id}")
+	public @ResponseBody Optional<Book> jsonBookById(@PathVariable("id") Long bookId) {
+		return bookRepository.findById(bookId);
 	}
 	
 }
